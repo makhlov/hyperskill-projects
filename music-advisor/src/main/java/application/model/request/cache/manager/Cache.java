a6cd8d8d6475f6a5dcccbe5c6c0023ac767160eb
Egor <makhlov.egor@gmail.com>
2021-12-16 19:56:24 +0300

    Minor fixes (delete duplicate code)

--- music-advisor/src/main/java/application/model/request/cache/manager/Cache.java
+++
@@ -1,8 +0,0 @@
-package application.model.request.cache.manager;
-
-import application.model.request.cache.exception.CacheExpiredException;
-
-public interface Cache<K, V> {
-    V get(final K key) throws CacheExpiredException;
-    void put(final K key, final V value);
-}
